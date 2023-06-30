d421005d378494d5d662a5b4f67e6d0ec7baf12a
Anders <achymake@gmail.com>
2023-06-30 20:57:04 +0200

    cleaned up

--- src/main/java/net/achymake/prevent/Prevent.java
+++ src/main/java/net/achymake/prevent/Prevent.java
@@ -1,16 +1,19 @@
 package net.achymake.prevent;
 
 import net.achymake.prevent.commands.MainCommand;
-import net.achymake.prevent.files.EntityData;
-import net.achymake.prevent.files.Message;
+import net.achymake.prevent.files.*;
 import net.achymake.prevent.listeners.*;
-import net.achymake.prevent.version.UpdateChecker;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Consumer;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 public final class Prevent extends JavaPlugin {
@@ -39,7 +42,7 @@ public final class Prevent extends JavaPlugin {
         commands();
         events();
         getMessage().sendLog(Level.INFO, "Enabled " + getName() + " " + getDescription().getVersion());
-        new UpdateChecker().getUpdate();
+        sendUpdate();
     }
     private void stop() {
         getServer().getScheduler().cancelTasks(this);
@@ -87,5 +90,45 @@ public final class Prevent extends JavaPlugin {
             saveConfig();
             getMessage().sendLog(Level.INFO, "created config.yml");
         }
+    }
+    public void sendUpdate(Player player) {
+        if (getConfig().getBoolean("notify-update.enable")) {
+            checkLatest((latest) -> {
+                if (!getDescription().getVersion().equals(latest)) {
+                    getMessage().send(player,"&6" + getName() + " Update:&f " + latest);
+                    getMessage().send(player,"&6Current Version: &f" + getDescription().getVersion());
+                }
+            });
+        }
+    }
+    public void sendUpdate() {
+        if (getConfig().getBoolean("notify-update.enable")) {
+            checkLatest((latest) -> {
+                getMessage().sendLog(Level.INFO, "Checking latest release");
+                if (getDescription().getVersion().equals(latest)) {
+                    getMessage().sendLog(Level.INFO, "You are using the latest version");
+                } else {
+                    getMessage().sendLog(Level.INFO, "New Update: " + latest);
+                    getMessage().sendLog(Level.INFO, "Current Version: " + getDescription().getVersion());
+                }
+            });
+        }
+    }
+    public void checkLatest(Consumer<String> consumer) {
+        getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            try {
+                InputStream inputStream = (new URL("https://api.spigotmc.org/legacy/update.php?resource=" + 110441)).openStream();
+                Scanner scanner = new Scanner(inputStream);
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                    scanner.close();
+                }
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                getMessage().sendLog(Level.WARNING, e.getMessage());
+            }
+        });
     }
 }
--- src/main/java/net/achymake/prevent/listeners/NotifyUpdate.java
+++ src/main/java/net/achymake/prevent/listeners/NotifyUpdate.java
@@ -1,19 +1,21 @@
 package net.achymake.prevent.listeners;
 
 import net.achymake.prevent.Prevent;
-import net.achymake.prevent.version.UpdateChecker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class NotifyUpdate implements Listener {
+    private Prevent getPlugin() {
+        return Prevent.getInstance();
+    }
     public NotifyUpdate(Prevent plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
     @EventHandler(priority = EventPriority.NORMAL)
     public void onNotifyUpdate(PlayerJoinEvent event) {
         if (!event.getPlayer().hasPermission("prevent.command"))return;
-        new UpdateChecker().sendMessage(event.getPlayer());
+        getPlugin().sendUpdate(event.getPlayer());
     }
 }
